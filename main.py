96095fe1a03f1ed9c9bbb27d660088f1beab2e3b
BnbBillionaire <99882254+BILLIMANCRYPTO@users.noreply.github.com>
2024-06-13 20:54:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ from tqdm import tqdm
 from collections import defaultdict
 from datetime import datetime
 
-
-# Значения, которые нужно адаптировать под ваше API
 API_KEY = 'SE6CI5YJBUJJX39SK9S3B3KWNH8194P222'
 BASE_URL = 'https://api.scrollscan.com/api'
 EXCEL_FILE = 'stats.xlsx'
-ETHER_PRICE = 3000  # Текущий курс эфира
 
-# Словарь для хранения информации о взаимодействии с контрактами
+# Функция для получения текущей цены эфира
+def get_current_eth_price():
+    url = 'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd'
+    response = requests.get(url)
+    response.raise_for_status()  # Проверка на ошибку запроса
+    price_data = response.json()
+    return price_data['ethereum']['usd']
+
+# Получение текущей цены эфира
+ETHER_PRICE = get_current_eth_price()
+
 contract_interactions = {
     'AAVE': {'address': '0xFF75A4B698E3Ec95E608ac0f22A03B8368E05F5D', 'count': 0},
     'Ambient': {'address': '0xaaaaAAAACB71BF2C8CaE522EA5fa455571A74106', 'count': 0},
@@ -40,6 +47,24 @@ def get_eth_balance(wallet_address):
     balance = response.json()
     eth_balance = int(balance['result']) / 10**18  # Переводим значение в эфир
     return eth_balance
+
+def get_token_balance(wallet_address, token_address):
+    url = f"{BASE_URL}?module=account&action=tokenbalance&contractaddress={token_address}&address={wallet_address}&tag=latest&apikey={API_KEY}"
+    response = requests.get(url)
+    response.raise_for_status()  # Проверка на ошибку запроса
+    balance = response.json()
+    token_balance = int(balance['result']) / 10**6  # Переводим значение в токены (предполагая 18 знаков после запятой)
+    return token_balance
+
+def parse_data(wallet_address):
+    url = f"https://kx58j6x5me.execute-api.us-east-1.amazonaws.com/scroll/bridge-balances?walletAddress={wallet_address}"
+    try:
+        response = requests.get(url)
+        data = response.json()
+        points_sum = sum(item.get("points", 0) for item in data)  # Суммируем значения points из каждого объекта
+        return points_sum
+    except Exception as e:
+        return f"Error: {e}"
 
 def get_transactions(wallet_address):
     url = f"{BASE_URL}?module=account&action=txlist&address={wallet_address}&startblock=0&endblock=99999999&sort=asc&apikey={API_KEY}"
@@ -72,10 +97,13 @@ def get_transactions(wallet_address):
                 if tx['to'].lower() == contract_info['address'].lower():
                     contract_interactions[contract_name]['count'] += 1
 
-    # Получаем баланс ETH для текущего кошелька
+    # Получаем баланс ETH, USDT и USDC для текущего кошелька
     eth_balance = get_eth_balance(wallet_address)
+    usdt_balance = get_token_balance(wallet_address, '0xf55BEC9cafDbE8730f096Aa55dad6D22d44099Df')
+    usdc_balance = get_token_balance(wallet_address, '0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4')
 
-    return eth_balance, total_contracts, total_eth_volume, len(unique_contracts), len(activity_by_month), len(activity_by_week), len(activity_by_day), list(contract_interactions.values())
+
+    return eth_balance, usdt_balance, usdc_balance, total_contracts, total_eth_volume, len(unique_contracts), len(activity_by_month), len(activity_by_week), len(activity_by_day), list(contract_interactions.values())
 
 # Чтение адресов кошельков из файла
 with open('wallets.txt', 'r') as file:
@@ -83,14 +111,15 @@ with open('wallets.txt', 'r') as file:
 
 wb = Workbook()
 ws = wb.active
-ws.append(['Wallet'] + ['ETH Balance', 'Total TX', 'Total USD Volume', 'Unique Contracts', 'Active Months', 'Active Weeks', 'Active Days'])
+ws.append(['Number', 'Wallet', 'ETH Balance', 'USDT Balance', 'USDC Balance', 'Total TX', 'Total USD Volume', 'Unique Contracts', 'Active Months', 'Active Weeks', 'Active Days', 'Points'])
 
 progress_bar = tqdm(total=len(wallet_addresses), desc='Processing wallets', position=0, leave=True)
 
-for wallet_address in wallet_addresses:
+for idx, wallet_address in enumerate(wallet_addresses, start=1):
     try:
-        eth_balance, total_tx, total_eth_volume, unique_contracts, active_months, active_weeks, active_days, contract_counts = get_transactions(wallet_address)
-        row_data = [wallet_address, eth_balance, total_tx, total_eth_volume * ETHER_PRICE, unique_contracts, active_months, active_weeks, active_days]
+        points_sum = parse_data(wallet_address)
+        eth_balance, usdt_balance, usdc_balance, total_tx, total_eth_volume, unique_contracts, active_months, active_weeks, active_days, contract_counts = get_transactions(wallet_address)
+        row_data = [idx, wallet_address, eth_balance, usdt_balance, usdc_balance, total_tx, total_eth_volume * ETHER_PRICE, unique_contracts, active_months, active_weeks, active_days, points_sum]
         ws.append(row_data)
     except Exception as e:
         print(f"Ошибка при обработке кошелька {wallet_address}: {str(e)}")
